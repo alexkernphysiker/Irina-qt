97930490fd0839bbe7c8669c30c7b34d188a90fa
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-08-27 14:53:12 +0200

    build script update

--- buildme.sh
+++ buildme.sh
@@ -3,6 +3,7 @@ cd MathLibs
 cd expression
 qmake expression.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
@@ -11,18 +12,21 @@ cd Core
 cd SortLib
 qmake SortLib.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd SortLibGui
 qmake SortLibGui.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd Irina-qt-executable
 qmake irina-qt-executable.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
@@ -31,6 +35,7 @@ cd DataOutput
 cd output-gnuplot
 qmake output-gnuplot.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
@@ -39,12 +44,14 @@ cd Plugins-DataInput
 cd format_text
 qmake format_text.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd formats_KINR
 qmake formats_KINR.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
@@ -53,24 +60,28 @@ cd Plugins-General
 cd DataGroup
 qmake DataGroup.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd FBKin
 qmake FBKin.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd Masks
 qmake Masks.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd NumericErrorsCalculation
 qmake NumericErrorsCalculation.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
@@ -79,12 +90,14 @@ cd Plugins-MonteCarlo
 cd Simulation
 qmake Simulation.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
 cd EnLos
 qmake EnLos.pro
 if (( $? )); then exit 1;fi
+make clean
 make
 if (( $? )); then exit 1;fi
 cd ..
