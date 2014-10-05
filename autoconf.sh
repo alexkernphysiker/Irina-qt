240eefecbac13dbeeba19bc31161b61d7ffaeb77
alexkernphysiker <alex_kernphysiker@privatdemail.net>
2014-10-05 17:57:06 +0200

    release

--- autoconf.sh
+++ autoconf.sh
@@ -1,5 +1,3 @@
 #/bin/bash
 mkdir bin 
 echo DESTDIR = $PWD/bin$'\n'DEPENDPATH += \$\$DESTDIR $'\n'QMAKE_CXXFLAGS+= -std=c++11$'\n'QMAKE_LFLAGS += -Wl,--rpath=\$\$DESTDIR > qt_build_path.pri
-git submodule init
-git submodule update
